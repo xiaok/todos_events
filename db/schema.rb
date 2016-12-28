86cd781dc4c7c6d0e4442f615ccc73c601cafb0b
xlaok <gaoyixin0618@foxmail.com>
2016-12-28 11:30:39 +0800

    create comment model and comments migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161228032438) do
+ActiveRecord::Schema.define(version: 20161228032938) do
 
   create_table "accesses", force: :cascade do |t|
     t.integer  "project_id"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 20161228032438) do
     t.datetime "updated_at", null: false
     t.index ["project_id"], name: "index_accesses_on_project_id"
     t.index ["user_id"], name: "index_accesses_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "todo_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["todo_id"], name: "index_comments_on_todo_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "projects", force: :cascade do |t|
