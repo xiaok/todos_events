aa36d15a77b3ff0a8d788a2fb96031be7574e447
xlaok <gaoyixin0618@foxmail.com>
2016-12-29 10:50:05 +0800

    belongs_to_required false; 删除 todos 里的 todo_list_id; 安装 simple_form; 增加一些 seed 数据;

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(name: 'yixin', avatar_url: Faker::Avatar.image)
+team = Team.create(name: 'team')
+project1 = team.projects.create(name: 'project1')
+project2 = team.projects.create(name: 'project2')
+access = Access.create(user: user, project: project1, role: 'admin')
+todo1 = project1.todos.create(name: '早上吃早饭')
+todo2 = project1.todos.create(name: '中午吃午饭', due_at: Time.now.to_i)
+todo3 = project1.todos.create(name: '晚上吃晚饭', is_complete: true)
+todo4 = project1.todos.create(name: '还要吃夜宵', assignee_id: user.id)
