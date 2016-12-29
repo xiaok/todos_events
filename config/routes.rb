8a61ab618ff4d398983017bca7a68c662aae97c9
xlaok <gaoyixin0618@foxmail.com>
2016-12-29 15:30:08 +0800

    更新 todo， 删除 todo，完成 todo； 单元测试；

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :projects, only: [:show] do
-    resources :todos
+    resources :todos, only: [:new, :create, :update]
   end
 
-  resources :todos, only: [:show, :edit] do
+  resources :todos, only: [:edit, :show, :destroy] do
     resources :comment
     post 'complete', to: 'todos#complete'
   end
--- spec/controller/todos_controller_spec.rb
+++ spec/controller/todos_controller_spec.rb
@@ -36,4 +36,33 @@ RSpec.describe TodosController, type: :controller do
       expect(response.body).to match(/#{comment.content}/)
     end
   end
+
+  describe 'update' do
+    before do
+      prepare_data
+    end
+
+    it "should update todos attributes" do
+      patch :update, params: { project_id: @todo1.project.id, id: @todo1.id, todo: { name: '修改后的name' } }
+      expect(@todo1.reload.name).to eq '修改后的name'
+    end
+
+    it "should not update project_id" do
+      patch :update, params: { project_id: @todo1.project.id, id: @todo1.id, todo: { project_id: 2 } }
+      expect(response).not_to be_success
+    end
+  end
+
+  describe 'destroy' do
+    before do
+      prepare_data
+    end
+
+    it "should destroy todo" do
+      xhr :delete, :destroy, params: { id: @todo1.id }
+
+      expect(response).to be_success
+      expect(Todo.find_by(id: @todo1.id)).to be_nil
+    end
+  end
 end
